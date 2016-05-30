35fa696a46cecceac28e3133fc39d8e7528d31a8
Yurii Chekan <gromret@gmail.com>
2016-05-30 23:40:45 +0300

    Substitution variable names

--- shpp/Task5/Word.java
+++ shpp/Task5/Word.java
@@ -1,6 +1,7 @@
 package com.shpp.task5.ychekan;
 
 import com.shpp.cs.a.console.TextProgram;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -9,19 +10,21 @@ import java.util.Scanner;
  * Created by ret284 on 30.05.2016.
  */
 
-public class Word extends TextProgram{
+public class Word extends TextProgram {
     private static final String DIRECTORY_FILE = "C:\\Users\\ret284\\IdeaProjects\\Task5\\src\\en-dictionary.txt";
-    public void run(){
-        while(true){
+
+    public void run() {
+        while (true) {
             String Letters = readLine("Enter your letters: ");
             println(searchWord(Letters));
         }
     }
+
     private String searchWord(String Letter) {
         String result = null;
         if (Letter.length() == 3) {
             /** The input stream are converted to lowercase characters and divide by */
-            char[] letter = Letter.toLowerCase().toCharArray();
+            char[] letterChar = Letter.toLowerCase().toCharArray();
             result = "";
             try (Scanner readerFile = new Scanner(new File(DIRECTORY_FILE))) {
                 ArrayList<String> list = new ArrayList<>();
@@ -31,10 +34,10 @@ public class Word extends TextProgram{
                 /** Check the letters on entering the word */
                 for (int i = 0; i < list.size() - 1; ++i) {
                     String str = list.get(i);
-                    char[] masChar = str.toCharArray();
+                    char[] arrayChar = str.toCharArray();
                     int k = 0;
-                    for (int j = 0; j < masChar.length - 1; ++j) {
-                        if (masChar[j] == letter[k]) {
+                    for (int j = 0; j < arrayChar.length - 1; ++j) {
+                        if (arrayChar[j] == letterChar[k]) {
                             ++k;
                             if (k == 3) {
                                 result += list.get(i) + "\n";
