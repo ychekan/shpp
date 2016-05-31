c30bc9a24919f302b6c167c6f8e765036e09a2f8
Yurii Chekan <gromret@gmail.com>
2016-05-31 03:35:50 +0300

    Task Parser - Done

--- shpp/Task5/Parsing.java
+++ shpp/Task5/Parsing.java
@@ -1,26 +1,28 @@
 package com.shpp.task5.ychekan;
 
 import com.shpp.cs.a.console.TextProgram;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * Created by ret284 on 30.05.2016.
+ * Created by ret284 on 31.05.2016.
  */
 public class Parsing extends TextProgram {
     private static final String MY_DIRECTORY = "C:\\Users\\ret284\\IdeaProjects\\Task5\\src\\";
 
     public void run() {
-        println(extractColumn("food-origins.csv", 1));
+        println(extractColumn("food-origins.csv", 0));
     }
 
     private ArrayList<String> extractColumn(String filename, int columnIndex) {
         String fileName = new String(MY_DIRECTORY + filename);
         ArrayList<String> listResult = new ArrayList<>();
-        String virgule = ",";
+        String virgule = ",";// Enter virgule
+        char kavichki = '"';
+        String startRes = "[";// for clarity
+        String endRes = "]";
         try (Scanner scan = new Scanner(new File(fileName))) {
             ArrayList<String> list = new ArrayList<>();
             while (scan.hasNext())
@@ -28,29 +30,27 @@ public class Parsing extends TextProgram {
             scan.close();
             String result = null;
             for (int i = 0; i < list.size(); ++i) {
-                //char[] charList = list.get(i).toCharArray();
-                String str = list.get(i);
-                char[] charList = str.toCharArray();
+                String srtingList = list.get(i);
+                char[] charList = srtingList.toCharArray();
                 result = "";
-                int startMassiv = columnIndex * str.indexOf(virgule);
-                int stopMassiv = virgule.indexOf(virgule, startMassiv + 1);
-
-                //int stopMassiv = lastIndexOf(virgule);
-                println(stopMassiv);
-                for (int k = startMassiv + 1; k < 10; ++k) {
-                    int indMassiv = str.indexOf(virgule);
+                int startMassiv = columnIndex * (srtingList.indexOf(virgule) + 1);
+                int check = srtingList.indexOf(kavichki);
+                for (int k = startMassiv; k < srtingList.length() - 1; ++k) {
+                    int indMassiv = srtingList.indexOf(virgule);
                     if (k == indMassiv) {
                         break;
                     }
+                    if (k == check) {
+                        charList[k] = charList[k + 1];
+                        ++k;
+                    }
                     result = result + charList[k];
                 }
-                listResult.add(result);
+                listResult.add(startRes + result + endRes);
             }
         } catch (FileNotFoundException e) {
             //e.printStackTrace();
         }
         return listResult;
     }
-
-
 }
