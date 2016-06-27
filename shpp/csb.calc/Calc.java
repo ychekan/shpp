3335277995527c36f1c0d271f09c5b80cf3ff22f
Yurii Chekan <gromret@gmail.com>
2016-06-27 23:24:24 +0300

    ok

---
+++ shpp/csb.calc/Calc.java
@@ -0,0 +1,36 @@
+package com.shpp.cs.calc;
+
+
+import com.shpp.cs.a.console.TextProgram;
+
+
+import java.util.Scanner;
+
+/**
+ * Created by ret284 on 20.06.2016.
+ */
+
+
+public class Calc extends TextProgram {
+    Scanner scan;
+
+    private void inputText() {
+        System.out.print("Please enter formula: ");
+        scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        String[] arr;
+        int indexEnd;
+        for (int i = 0; str.length(); ++i) {
+            int ind = str.indexOf();
+            arr.push(str.substring());
+            indexEnd
+
+        }
+        ind
+        System.out.print(strScan);
+    }
+
+    public void run() {
+        inputText();
+    }
+}
