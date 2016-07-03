2cf7176e7e28310b30fc7ca1b3c52ca6d5d38a9f
Yurii Chekan <gromret@gmail.com>
2016-07-03 17:14:22 +0300

    Done

--- shpp/Task5/Parsing.java
+++ shpp/Task5/Parsing.java
@@ -1,56 +1,69 @@
 package com.shpp.task5.ychekan;
 
 import com.shpp.cs.a.console.TextProgram;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * Created by ret284 on 31.05.2016.
+ * Created by ret284 on 03.07.2016.
  */
+
 public class Parsing extends TextProgram {
+    /**
+     * Directory for file .scv
+     * */
     private static final String MY_DIRECTORY = "C:\\Users\\ret284\\IdeaProjects\\Task5\\src\\";
 
     public void run() {
-        println(extractColumn("food-origins.csv", 0));
+        //println(extractColumn("food-origins.csv", 0));
+        println(extractColumn("food-origins.csv", 1));
     }
 
+    /**
+     * The method for read and separation file
+     */
     private ArrayList<String> extractColumn(String filename, int columnIndex) {
-        String fileName = new String(MY_DIRECTORY + filename);
-        ArrayList<String> listResult = new ArrayList<>();
-        String virgule = ",";// Enter virgule
-        char kavichki = '"';
-        String startRes = "[";// for clarity
-        String endRes = "]";
-        try (Scanner scan = new Scanner(new File(fileName))) {
+        try {
+            ArrayList<String> result = new ArrayList<>();
             ArrayList<String> list = new ArrayList<>();
+            Scanner scan = new Scanner(new File(MY_DIRECTORY + filename));
             while (scan.hasNext())
                 list.add(scan.nextLine());
             scan.close();
-            String result = null;
+            // The check on the splitter dividing line
             for (int i = 0; i < list.size(); ++i) {
-                String srtingList = list.get(i);
-                char[] charList = srtingList.toCharArray();
-                result = "";
-                int startMassiv = columnIndex * (srtingList.indexOf(virgule) + 1);
-                int check = srtingList.indexOf(kavichki);
-                for (int k = startMassiv; k < srtingList.length() - 1; ++k) {
-                    int indMassiv = srtingList.indexOf(virgule);
-                    if (k == indMassiv) {
-                        break;
-                    }
-                    if (k == check) {
-                        charList[k] = charList[k + 1];
-                        ++k;
-                    }
-                    result = result + charList[k];
-                }
-                listResult.add(startRes + result + endRes);
+                result.add(checkStr(list.get(i)).get(columnIndex));
             }
+            return result;
         } catch (FileNotFoundException e) {
-            //e.printStackTrace();
+            e.printStackTrace();
         }
-        return listResult;
+        return null;
+    }
+
+    /**
+     * The method for correct separation line in the cell
+     */
+    private ArrayList<String> checkStr(String str) {
+        ArrayList<String> result = new ArrayList<>();
+        String cell = "";
+        String[] limit = str.split(",");
+        for (int i = 0; i < limit.length; i++) {
+            if (limit[i].charAt(0) == '\"' && limit[i].charAt(limit[i].length() - 1) != '\"')
+                cell = limit[i];
+            if (limit[i].charAt(0) != '\"' && limit[i].charAt(limit[i].length() - 1) == '\"') {
+                cell = cell + "," + limit[i];
+                result.add(cell);
+                cell = "";
+            }
+            if (cell != "")
+                cell = limit[i];
+            else
+                result.add(limit[i]);
+        }
+        return result;
     }
 }
