141d4fe3fb0d91d44918b8ae30689cac1969b3a5
Yurii Chekan <gromret@gmail.com>
2016-07-03 13:56:28 +0300

    Reading file removal in a separate method, and adjusted the warning message in the absence of a match result

--- shpp/Task5/Word.java
+++ shpp/Task5/Word.java
@@ -14,46 +14,54 @@ public class Word extends TextProgram {
     private static final String DIRECTORY_FILE = "C:\\Users\\ret284\\IdeaProjects\\Task5\\src\\en-dictionary.txt";
 
     public void run() {
+        ArrayList<String> arrList = readDataFile();
         while (true) {
-            String Letters = readLine("Enter your letters: ");
-            println(searchWord(Letters));
+            String inputLetter = readLine("Enter your letters: ");
+            println(searchWord(inputLetter, arrList));
         }
     }
 
-    private String searchWord(String Letter) {
+    private String searchWord(String Letter, ArrayList<String> arrList) {
         String result = null;
         if (Letter.length() == 3) {
             /** The input stream are converted to lowercase characters and divide by */
             char[] letterChar = Letter.toLowerCase().toCharArray();
             result = "";
-            try (Scanner readerFile = new Scanner(new File(DIRECTORY_FILE))) {
-                ArrayList<String> list = new ArrayList<>();
-                while (readerFile.hasNext())
-                    list.add(readerFile.nextLine());
-                readerFile.close();
-                /** Check the letters on entering the word */
-                for (int i = 0; i < list.size() - 1; ++i) {
-                    String str = list.get(i);
-                    char[] arrayChar = str.toCharArray();
-                    int k = 0;
-                    for (int j = 0; j < arrayChar.length - 1; ++j) {
-                        if (arrayChar[j] == letterChar[k]) {
-                            ++k;
-                            if (k == 3) {
-                                result += list.get(i) + "\n";
-                                break;
-                            }
+            for (int i = 0; i < arrList.size() - 1; ++i) {
+                //String str = arrList.get(i);
+                char[] arrayChar = arrList.get(i).toCharArray();
+                int k = 0;
+                for (int j = 0; j < arrayChar.length - 1; ++j) {
+                    if (arrayChar[j] == letterChar[k]) {
+                        ++k;
+                        if (k == 3) {
+                            result += arrList.get(i) + "\n";
+                            break;
                         }
                     }
                 }
-                println("Sorry. No there is no match");
-            } catch (FileNotFoundException e) {
-                //e.printStackTrace();
+            }
+            if (result.length() == 0) {
+                result = "Sorry, Words such letters is not! :-(";
             }
         } else
-            println("You have entered more or less 3 symbols");
+            result = "You have entered more or less 3 symbols";
         return result;
     }
 
-
+    /**
+     * The method for read file and push in array
+     */
+    private ArrayList<String> readDataFile() {
+        try (Scanner readerFile = new Scanner(new File(DIRECTORY_FILE))) {
+            ArrayList<String> list = new ArrayList<>();
+            while (readerFile.hasNext())
+                list.add(readerFile.nextLine());
+            readerFile.close();
+            return list;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
